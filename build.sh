58c744932b93fa15028f66d2c5029d219471317e
Ivaylo Iliev <ivailo.iliev@nextforcedev.com>
2020-03-05 11:10:10 +0200

    Replaced wget with curl

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ EXISTING_SHA256SUM=`cat PKGBUILD|egrep "^sha256sums_x86_64"`
 rm -rf mailspring-*-amd64.deb mailspring-*.pkg.tar.xz pkg src
 
 sed -i "s/$PKGBUILD_VERSION/pkgver=$LATEST_RELEASE/" PKGBUILD
-wget "https://github.com/Foundry376/Mailspring/releases/download/$LATEST_RELEASE/mailspring-$LATEST_RELEASE-amd64.deb"
+curl -L "https://github.com/Foundry376/Mailspring/releases/download/$LATEST_RELEASE/mailspring-$LATEST_RELEASE-amd64.deb" --output "mailspring-$LATEST_RELEASE-amd64.deb"
 SHA256SUM=`sha256sum "mailspring-$LATEST_RELEASE-amd64.deb"|cut -d' ' -f1`
 sed -i "s/$EXISTING_SHA256SUM/sha256sums_x86_64=('$SHA256SUM')/" PKGBUILD
 
